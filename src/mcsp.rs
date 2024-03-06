b6c9c5de5581550664af627f8ced275c3625ac9d
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2024-03-06 14:13:43 +0100

    Better format

--- src/mcsp.rs
+++ src/mcsp.rs
@@ -1,28 +1,26 @@
-use std::{
-    collections::{HashMap, HashSet},
-};
-use std::fmt::Debug;
-use std::marker::PhantomData;
+use crate::input_graph::Node::{Action, State};
+use crate::input_graph::{ApMap, InputGraph, Node, ParseImpl, MDP};
+use crate::logic::{parse_formula, Formula};
+use crate::utils::file::read_file;
+use crate::Args;
 use log::info;
 use petgraph::dot::Dot;
-use petgraph::graph::{NodeIndex};
-use crate::{Args};
-use crate::input_graph::{ApMap, InputGraph, MDP, Node, ParseImpl};
-use crate::input_graph::Node::{Action, State};
-use crate::logic::{Formula, parse_formula};
-use crate::utils::file::read_file;
+use petgraph::graph::NodeIndex;
+use std::collections::{HashMap, HashSet};
+use std::fmt::Debug;
+use std::marker::PhantomData;
 
 pub struct ModelCheckInfo<'a, T> {
     pub reach_graph: MDP<T>,
     pub ap_map: &'a ApMap<T>,
     pub formula: Box<dyn Formula>,
-    pub max_error: f64
+    pub max_error: f64,
 }
 
 pub struct PctlInfo {
     pub reach_graph: MDP<NodeIndex>,
     pub ap_map: HashMap<String, HashSet<NodeIndex>>,
-    pub max_error: f64
+    pub max_error: f64,
 }
 
 pub struct ModelCheck<T: InputGraph, P: ParseImpl<T>> {
@@ -30,7 +28,11 @@ pub struct ModelCheck<T: InputGraph, P: ParseImpl<T>> {
     p2: PhantomData<P>,
 }
 
-impl<T, P> ModelCheck<T, P> where T: InputGraph, P: ParseImpl<T>{
+impl<T, P> ModelCheck<T, P>
+where
+    T: InputGraph,
+    P: ParseImpl<T>,
+{
     //noinspection RsTraitObligations
     pub fn start(args: Args) {
         info!("Parsing input petri net");
@@ -51,7 +53,7 @@ impl<T, P> ModelCheck<T, P> where T: InputGraph, P: ParseImpl<T>{
         info!("Parsing formula...");
         let formula = parse_formula(args.logic_type, &content);
         info!("Formula parsed successfully");
-        let mc: ModelCheckInfo<T::S> = ModelCheckInfo{
+        let mc: ModelCheckInfo<T::S> = ModelCheckInfo {
             reach_graph,
             ap_map: input_graph.get_ap_map(),
             formula,
@@ -60,20 +62,23 @@ impl<T, P> ModelCheck<T, P> where T: InputGraph, P: ParseImpl<T>{
         Self::evaluate_pctl(mc);
     }
 
-    pub fn evaluate_pctl<K>(mc_info: ModelCheckInfo<K>) where K: Debug + PartialEq + Clone {
+    pub fn evaluate_pctl<K>(mc_info: ModelCheckInfo<K>)
+    where
+        K: Debug + PartialEq + Clone,
+    {
         let normalized_mdp = mc_info.reach_graph.map(
-            |ni,node| match node {
-                State(_) => {State(ni)}
-                Action(a) => {Action(a.into())}
+            |ni, node| match node {
+                State(_) => State(ni),
+                Action(a) => Action(a.into()),
             },
-            |_,e| *e
+            |_, e| *e,
         );
 
         let normalized_ap_map = Self::normalize_ap_map(&mc_info.reach_graph, mc_info.ap_map);
-        let pctl_info: PctlInfo = PctlInfo{
+        let pctl_info: PctlInfo = PctlInfo {
             reach_graph: normalized_mdp,
             ap_map: normalized_ap_map,
-            max_error: mc_info.max_error
+            max_error: mc_info.max_error,
         };
 
         info!("Evaluating formula...");
@@ -81,7 +86,8 @@ impl<T, P> ModelCheck<T, P> where T: InputGraph, P: ParseImpl<T>{
 
         // Print all markings satisfying the pctl statement
         info!("The following markings satisfy the given pctl statement:");
-        info!("{:?}",
+        info!(
+            "{:?}",
             markings
                 .iter()
                 .map(|index| mc_info.reach_graph[*index].clone())
@@ -92,16 +98,21 @@ impl<T, P> ModelCheck<T, P> where T: InputGraph, P: ParseImpl<T>{
     fn normalize_ap_map<K>(
         graph: &MDP<K>,
         src_map: &HashMap<String, HashSet<K>>,
-    ) -> HashMap<String, HashSet<NodeIndex>> where K: PartialEq{
-
+    ) -> HashMap<String, HashSet<NodeIndex>>
+    where
+        K: PartialEq,
+    {
         let mut normalized_ap_map: HashMap<String, HashSet<NodeIndex>> = HashMap::new();
         for (ap, markings) in src_map {
             normalized_ap_map.insert(ap.into(), HashSet::new());
             for marking in markings {
-                let node_index = graph.node_indices().find(|&node_index| match &graph[node_index] {
-                    State(m) => m == marking,
-                    Action(_) => false
-                }).unwrap();
+                let node_index = graph
+                    .node_indices()
+                    .find(|&node_index| match &graph[node_index] {
+                        State(m) => m == marking,
+                        Action(_) => false,
+                    })
+                    .unwrap();
                 normalized_ap_map.get_mut(ap).unwrap().insert(node_index);
             }
         }
