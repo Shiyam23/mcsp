6b050d8dfb6dfebcf803ec4898ad7b11aff3e6ad
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2024-03-24 03:02:48 +0100

    Remove redundant transitions from ba

--- src/logic/ltl/ba.rs
+++ src/logic/ltl/ba.rs
@@ -22,7 +22,11 @@ pub fn to_ba(gba: GBA) {
     acc_transitions.sort_by(|s2, s1| s1.len().cmp(&s2.len()));
     pop_queue.extend(initials);
     while let Some(state) = pop_queue.pop_front() {
+        println!("State: {:?}", state);
         let transitions = delta(&state, &gba.trans_f, &acc_transitions);
+        for transition in &transitions {
+            println!("T: {} -> {:?}", transition.props, transition.target);
+        }
         trans_f.insert(state.clone(), transitions);
         for transition in trans_f.get(&state).unwrap() {
             let target = transition.target.clone();
@@ -38,25 +42,17 @@ fn delta(
     old_delta: &HashMap<String, SimpleTransitions>,
     acc_t: &Vec<HashSet<(String, SimpleTransition)>>,
 ) -> HashSet<Transition> {
-    old_delta
-        .get(&state.0)
-        .unwrap()
-        .iter()
-        .map(|t| {
-            let next_j = next(acc_t, state.1, &(state.0.clone(), t.clone()));
-            let tran = Transition {
-                props: t.props.clone(),
-                target: (t.target.clone(), next_j),
-            };
-            tran
-        })
-        .inspect(|f| {
-            println!(
-                "({}, {}) -> {} -> ({}, {})",
-                state.0, state.1, f.props, f.target.0, f.target.1
-            )
-        })
-        .collect()
+    let mut result: HashSet<Transition> = HashSet::new();
+    let delta_state = old_delta.get(&state.0).unwrap();
+    for t in delta_state {
+        let next_j = next(acc_t, state.1, &(state.0.clone(), t.clone()));
+        let tran = Transition {
+            props: t.props.clone(),
+            target: (t.target.clone(), next_j),
+        };
+        result.insert(tran);
+    }
+    prune_transitions(result)
 }
 
 fn next(
@@ -78,4 +74,15 @@ fn next(
         })
         .max()
         .unwrap_or(start_index);
+}
+
+fn prune_transitions(transitions: HashSet<Transition>) -> HashSet<Transition> {
+    let copy = transitions.clone();
+    copy.into_iter()
+        .filter(|t| {
+            !transitions
+                .iter()
+                .any(|ot| t != ot && t.target == ot.target && ot.props.0.is_subset(&t.props.0))
+        })
+        .collect()
 }
